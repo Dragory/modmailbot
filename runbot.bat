21d91afa4d9970b0210775818db9f22162933776
Ezra Dulaney <dulaneyezra@gmail.com>
2017-08-08 16:08:36 -0400

    Update runbot.bat

--- runbot.bat
+++ runbot.bat
@@ -1,6 +1,15 @@
 @echo off
-cd %~dp0
-npm install
+Echo Hello, I will check for updates then run the bot.
+timeout /nobreak /t 3
 cls
+cd %~dp0
+echo Checking For Updates!
+echo.
+call npm install
+echo.
+:: I had to put a timeout here so people can see if the update left any errors.
+timeout /t 10
+cls
+echo Starting Bot!
 npm start
 pause
