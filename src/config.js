dcd9d947343a988d4f385422081d764e1eaf1f3f
Dragory <dragory.up@gmail.com>
2018-03-11 22:08:59 +0200

    Fix incorrect mentionRole default

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ const defaultConfig = {
   "responseMessage": "Thank you for your message! Our mod team will reply to you here as soon as possible.",
 
   "newThreadCategoryId": null,
-  "mentionRole": null,
+  "mentionRole": "here",
 
   "inboxServerPermission": null,
   "alwaysReply": false,
