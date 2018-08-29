5a760b5e506a3121c60169a6b8b2650c2cf7bdd6
Dylan Akhawais <dylanakhawais@gmail.com>
2018-08-29 02:30:12 +0100

    Moving the 'getAllUsers' option into the correct parameter object

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,13 @@
 const Eris = require('eris');
 const config = require('./config');
 
-const bot = new Eris.CommandClient(config.token, {}, {
+const bot = new Eris.CommandClient(config.token, {
+  getAllUsers: true,
+}, {
   prefix: config.prefix,
   ignoreSelf: true,
   ignoreBots: true,
   defaultHelpCommand: false,
-  getAllUsers: true,
   defaultCommandOptions: {
     caseInsensitive: true,
   },
