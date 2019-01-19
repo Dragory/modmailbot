82d4b3e5b43b37b71a34238b6b196b48c8ac8c38
reboxer <7484116+reboxer@users.noreply.github.com>
2019-01-19 11:49:43 -0300

    Update logsfile.js
    Even if this is not going to get merged (or at least not yet), just wanted to improve that garbage code i wrote

--- src/data/logsfile.js
+++ src/data/logsfile.js
@@ -4,12 +4,12 @@ const bot = require('../bot');
 
 const folder = path.join(__dirname, 'logsfile');
 
-const threads = require('./threads');
 const utils = require('../utils');
 const config = require('../config');
+const threads = require('./threads');
 
     bot.on('messageCreate', async msg => {
-        if ( (config.useLogFile) === false ) return;
+        if (! config.useLogFile) return;
 
         // Check if the message was sent in the inbox server
         if (! utils.messageIsOnInboxServer(msg)) return;
@@ -19,15 +19,15 @@ const config = require('../config');
         if (! thread) return;
      
         if (thread) {
-           // wait for the getDMChannel function to complete and then return the available user info, such as ID, username, discriminator, etc.
-           let userData = ( await thread.getDMChannel() ).recipient;
+           // Wait for the getDMChannel function to complete and then return the available user info, such as ID, username, discriminator, etc.
+           let userData = (await thread.getDMChannel()).recipient;
            exists(msg, userData);
        }
      });
 
      bot.on('channelDelete', async (channel) => {
-         if ( (config.useLogFile) === false ) return;
-         if ( (config.logFileChannelID) === null ) {
+         if (! config.useLogFile) return;
+         if (! config.logFileChannelID) {
             console.log('[WARN] Can not upload log file to discord, a channel id for logFileChannelID has not been set!')
             return;
          }
@@ -39,7 +39,7 @@ const config = require('../config');
          
         if (thread) {
             console.log(`[NOTE] Thread channel closed, uploading log file for ${channel.name}...`)
-            await uploadFile(channel.id)
+            uploadFile(channel.id)
         }
       });
 
@@ -48,33 +48,31 @@ async function uploadFile(threadChannelID) {
 
     try {
     const thread = await threads.findByChannelId(threadChannelID);
-    let data = ( await thread.getDMChannel() ).recipient;
-    let userID = ( await data.id);
+    let data = (await thread.getDMChannel()).recipient;
+    let userID = data.id;
 
-    let username = await (`${data.username}#${data.discriminator}`);
+    let username = `${data.username}#${data.discriminator}`;
     let fileChannelID = config.logFileChannelID;
     let logChannelID = config.logChannelId
 
-        await fs.readFile(`${folder}/${username}.txt`, 'utf8', async function (err,data) {
-            if (err) {
-              return console.log(err);
+        fs.readFile(`${folder}/${username}.txt`, 'utf8', (error,data) => {
+            if (error) {
+              return console.log(error);
             }
 
-            await bot.createMessage(fileChannelID,`Logs for ${username} (${userID})`, {file: Buffer.from(data), name: `${username}.txt`}).then( async (msg) => {
+            bot.createMessage(fileChannelID,`Logs for ${username} (${userID})`, {file: Buffer.from(data), name: `${username}.txt`}).then((msg) => {
 
-                let attachmentURL = await msg.attachments[0].url.split("/");
+                let attachmentURL = msg.attachments[0].url.split("/");
+                let channelID = attachmentURL[4];
+                let id = attachmentURL[5];
+                let filename = msg.attachments[0].filename.split(".")[0];
         
-                let channelID = await attachmentURL[4];
-        
-                let id = await attachmentURL[5];
-        
-                let filename = await msg.attachments[0].filename.split(".")[0];
-        
-                await bot.createMessage(logChannelID,`Logs: http://txt.discord.website/?txt=${channelID}/${id}/${filename}`)
+                bot.createMessage(logChannelID,`Logs: http://txt.discord.website/?txt=${channelID}/${id}/${filename}`).catch(error => console.error);
                 console.log(`[NOTE] Succesfully uploaded file to discord`)
-            }).catch( (error) => {
+            }).catch((error) => {
                 if (error.code === 10003) {
                     console.log('[WARN] Could not upload file, the specified LogFileChannelID is not a valid channel id!')
+                    return;
                 } else { 
                     console.log(error)
                     return;
@@ -97,35 +95,32 @@ async function uploadFile(threadChannelID) {
 };
 
 // Check if the .txt file exists, if exists, append the file with the messages sent, if not, create a new one
-async function exists(msg, user) {
+
+function exists(msg, user) {
     try {
-    let checkFile = await (`${user.username}#${user.discriminator}`);
+    let checkFile = `${user.username}#${user.discriminator}`;
 
-    if ( (fs.existsSync(`${folder}/${checkFile}.txt`)) === true) {
-        await append(msg, checkFile);
+    if ((fs.existsSync(`${folder}/${checkFile}.txt`))) {
+        append(msg, checkFile);
     } else {
-        console.log(`[NOTE] Creating new text file log for ${checkFile}...`)
-        await firstWrite(msg, user, checkFile);
+        console.log(`[NOTE] Creating new text file log for ${checkFile}...`);
+        firstWrite(msg, user, checkFile);
     };
-
     } catch(error) {
-        console.log(error)
+        console.error(error)
     }
 };
 
 // Create the file with the name username#discriminator and write the header to the file
-async function firstWrite(msg, user, checkFile) {
-
+function firstWrite(msg, user, checkFile) {
     try {
-
-        let firstWriteText = await `-- Logs -- ${user.username}#${user.discriminator} (${user.id})\r\n`+
+        let firstWriteText = `-- Logs -- ${user.username}#${user.discriminator} (${user.id})\r\n`+
         `\r\n[${(new Date(msg.timestamp)).toUTCString()}] ${msg.member.username}#${msg.member.discriminator} (${msg.member.id}) : ${msg.content}\r\n`;
     
-        fs.writeFile(`${folder}/${checkFile}.txt`, firstWriteText, function(writeError) {
+        fs.writeFile(`${folder}/${checkFile}.txt`, firstWriteText, (writeError) => {
             if (writeError) return console.log(writeError);
 
         console.log(`[NOTE] Succesfully created new log file for ${checkFile}`);
-
     });
 
     } catch(error) {
@@ -134,11 +129,11 @@ async function firstWrite(msg, user, checkFile) {
 };
 
 // Append the message to the .txt file if it exists, if not, a new one will be created without the header
-async function append(msg, checkFile) {
+function append(msg, checkFile) {
     try {
-        let content = await `\r\n[${(new Date(msg.timestamp)).toUTCString()}] ${msg.member.username}#${msg.member.discriminator} (${msg.member.id}) : ${msg.content}\r\n`;
+        let content = `\r\n[${(new Date(msg.timestamp)).toUTCString()}] ${msg.member.username}#${msg.member.discriminator} (${msg.member.id}) : ${msg.content}\r\n`;
 
-        fs.appendFile(`${folder}/${checkFile}.txt`, content, function(error) {
+        fs.appendFile(`${folder}/${checkFile}.txt`, content, (error) => {
             if (error) {
                 console.log(error);
                 return;
