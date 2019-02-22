9c9bb743e7f65c5689ac92ee96934f221e804a2b
reboxer <7484116+reboxer@users.noreply.github.com>
2019-02-22 15:50:01 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -176,7 +176,7 @@ bot.on('messageCreate', async msg => {
 
   // Send an auto-response to the mention, if enabled
   if (config.botMentionResponse) {
-    bot.createMessage(msg.channel.id, config.botMentionResponse);
+    bot.createMessage(msg.channel.id, config.botMentionResponse.replace(/{userMention}/g, `<@${msg.author.id}>`);
   }
 });
 
