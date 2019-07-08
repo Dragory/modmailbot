d91e569725de0f15ea4d35a2ac8b534b15717813
Robert Klebes <robert@klebes.info>
2019-07-08 11:35:04 -0400

    Add direct link to message onBotMention
    Implement feature request #318
    Add a blank line and then direct link to the individual message in which
    the bot was mentioned.

--- src/main.js
+++ src/main.js
@@ -209,10 +209,11 @@ function initBaseMessageHandlers() {
     const staffMention = (config.pingOnBotMention ? utils.getInboxMention() : '');
 
     if (mainGuilds.length === 1) {
-      content = `${staffMention}Bot mentioned in ${msg.channel.mention} by **${msg.author.username}#${msg.author.discriminator}**: "${msg.cleanContent}"`;
+    content = `${staffMention}Bot mentioned in ${msg.channel.mention} by **${msg.author.username}#${msg.author.discriminator}**: "${msg.cleanContent}"\n\nhttps:\/\/discordapp.com\/channels\/${msg.channel.guild.id}\/$msg.channel.id}\/${msg.id}`;
     } else {
-      content = `${staffMention}Bot mentioned in ${msg.channel.mention} (${msg.channel.guild.name}) by **${msg.author.username}#${msg.author.discriminator}**: "${msg.cleanContent}"`;
+    content = `${staffMention}Bot mentioned in ${msg.channel.mention} (${msg.channel.guild.name}) by **${msg.author.username}#${msg.author.discriminator}**: "${msg.cleanContent}"https:\/\/discordapp.com\/channels\/${msg.channel.guild.id}\/$msg.channel.id}\/${msg.id}`;
     }
+
 
     bot.createMessage(utils.getLogChannel().id, {
       content,
