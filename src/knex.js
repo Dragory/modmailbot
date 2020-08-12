1f5faaab5b61b03a16d960e5705e176db94c1789
Dragory <2606411+Dragory@users.noreply.github.com>
2020-08-13 02:44:42 +0300

    Fix unnecessary warning about migrations from knex

--- src/knex.js
+++ src/knex.js
@@ -1,2 +1,13 @@
 const config = require("./cfg");
-module.exports = require("knex")(config.knex);
+module.exports = require("knex")({
+  ...config.knex,
+  log: {
+    warn(message) {
+      if (message.startsWith("FS-related option specified for migration configuration")) {
+        return;
+      }
+
+      console.warn(message);
+    },
+  },
+});
