875e70ed3b9eb6a3d7a17bf3bf25bbb30036ffe7
bycop <thebycop@gmail.com>
2021-03-30 19:01:07 +0200

    Add !shownote on thread to show directly the thread user note

--- src/modules/shownote.js
+++ src/modules/shownote.js
@@ -14,7 +14,7 @@ module.exports = ({ bot, knex, config, commands }) => {
   commands.addInboxServerCommand("shownote", "<userId:userId>", async (msg, args, thread) => {
     const user = bot.users.get(args.userId)
     if (! user) {
-      msg.channel.createMessage("User not found!");
+      utils.postError(msg.channel, "User not found")
       return;
     }
     let notes = JSON.parse(fs.readFileSync("./logs/notes/notes.json", "utf8"));
@@ -24,6 +24,34 @@ module.exports = ({ bot, knex, config, commands }) => {
     }
     if (found !== -1 && notes[user.id].note !== "undefined") {
       msg.channel.createMessage("Note for <@" + user.id + "> : `" + notes[user.id].note + "`")
+    }
+    else {
+      msg.channel.createMessage("No note for this user.")
+    }
+  })
+  commands.addInboxThreadCommand("shownote", [{ name: "userId", type: "userId", required: false}], async (msg, args, thread) => {
+    let user;
+    if (args.userId){
+      user = bot.users.get(args.userId)
+      if (!user){
+        utils.postError(msg.channel, "User not found")
+        return ;
+      }
+      else
+      {
+        user = user.id;
+      }
+    }
+    if (! user || !args.userId) {
+      user = thread.user_id;
+    }
+    let notes = JSON.parse(fs.readFileSync("./logs/notes/notes.json", "utf8"));
+    let found = -1;
+    if (notes[user]) {
+      found = 1;
+    }
+    if (found !== -1 && notes[user].note !== "undefined") {
+      msg.channel.createMessage("Note for <@" + user + "> : `" + notes[user].note + "`")
     }
     else {
       msg.channel.createMessage("No note for this user.")
