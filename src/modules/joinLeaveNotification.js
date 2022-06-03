deb72880cc02e11ae334d14829db7b5b6731eb00
Lena <7890309+DarkView@users.noreply.github.com>
2022-06-03 22:56:54 +0200

    Add ban/unban detection to join/leave notifications (#600)

--- src/modules/joinLeaveNotification.js
+++ src/modules/joinLeaveNotification.js
@@ -3,29 +3,73 @@ const threads = require("../data/threads");
 const utils = require("../utils");
 
 module.exports = ({ bot }) => {
-    // Join Notification: Post a message in the thread if the user joins a main server
-    if (config.notifyOnMainServerJoin) {
-        bot.on("guildMemberAdd", async (guild, member) => {
-            const mainGuilds = utils.getMainGuilds();
-            if (! mainGuilds.find(gld => gld.id === guild.id)) return;
+  const leaveIgnoreIDs = [];
 
-            const thread = await threads.findOpenThreadByUserId(member.id);
-            if (thread != null) {
-                await thread.postSystemMessage(`***The user joined the guild ${guild.name}.***`);
-            }
-        });
-    }
+  // Join Notification: Post a message in the thread if the user joins a main server
+  if (config.notifyOnMainServerJoin) {
+    bot.on("guildMemberAdd", async (guild, member) => {
+      const mainGuilds = utils.getMainGuilds();
+      if (! mainGuilds.find((gld) => gld.id === guild.id)) return;
 
-    // Leave Notification: Post a message in the thread if the user leaves a main server
-    if (config.notifyOnMainServerLeave) {
-        bot.on("guildMemberRemove", async (guild, member) => {
-            const mainGuilds = utils.getMainGuilds();
-            if (! mainGuilds.find(gld => gld.id === guild.id)) return;
+      const thread = await threads.findOpenThreadByUserId(member.id);
+      if (thread != null) {
+        await thread.postSystemMessage(
+          `***The user joined the ${guild.name} server.***`
+        );
+      }
+    });
+  }
 
-            const thread = await threads.findOpenThreadByUserId(member.id);
-            if (thread != null) {
-                await thread.postSystemMessage(`***The user left the guild ${guild.name}.***`);
-            }
-        });
-    }
+  // Leave Notification: Post a message in the thread if the user leaves a main server
+  if (config.notifyOnMainServerLeave) {
+    bot.on("guildMemberRemove", async (guild, member) => {
+      const mainGuilds = utils.getMainGuilds();
+      if (! mainGuilds.find((gld) => gld.id === guild.id)) return;
+
+      // Ensure that possible ban events are caught before sending message (race condition)
+      setTimeout(async () => {
+        const thread = await threads.findOpenThreadByUserId(member.id);
+        if (thread != null) {
+          if (leaveIgnoreIDs.includes(member.id)) {
+            leaveIgnoreIDs.splice(leaveIgnoreIDs.indexOf(member.id), 1);
+          } else {
+            await thread.postSystemMessage(
+              `***The user left the ${guild.name} server.***`
+            );
+          }
+        }
+      }, 2 * 1000);
+    });
+  }
+
+  // Leave Notification: Post a message in the thread if the user is banned from a main server
+  if (config.notifyOnMainServerLeave) {
+    bot.on("guildBanAdd", async (guild, user) => {
+      const mainGuilds = utils.getMainGuilds();
+      if (! mainGuilds.find((gld) => gld.id === guild.id)) return;
+
+      const thread = await threads.findOpenThreadByUserId(user.id);
+      if (thread != null) {
+        await thread.postSystemMessage(
+          `***The user was banned from the ${guild.name} server.***`
+        );
+        leaveIgnoreIDs.push(user.id);
+      }
+    });
+  }
+
+  // "Join" Notification: Post a message in the thread if the user is unbanned from a main server
+  if (config.notifyOnMainServerJoin) {
+    bot.on("guildBanRemove", async (guild, user) => {
+      const mainGuilds = utils.getMainGuilds();
+      if (! mainGuilds.find((gld) => gld.id === guild.id)) return;
+
+      const thread = await threads.findOpenThreadByUserId(user.id);
+      if (thread != null) {
+        await thread.postSystemMessage(
+          `***The user was unbanned from the ${guild.name} server.***`
+        );
+      }
+    });
+  }
 };
