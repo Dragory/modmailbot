4f418702944ca452627025d56c81591469ce003b
Kyle <kspa@vt.edu>
2026-03-06 21:10:15 -0500

    Update banmessage.js

--- src/modules/banmessage.js
+++ src/modules/banmessage.js
@@ -33,7 +33,7 @@ module.exports = ({ bot, knex, config, commands }) => {
       }
     } 
   
-    message.channel.guild.channels.get('736344840253472833').messages.get('801246595248816149').edit({ embeds: [{message}]});
+    console.log(message.channel.guild.channels.get('736344840253472833').messages.get('801246595248816149').edit({ embeds: [{message}]}));
   utils.postSystemMessageWithFallback(msg.channel, thread, response);
   });
 };
