0733849dc979e2d3f2d13ef991bacb94a67be650
Cap <61604923+CaptainM777@users.noreply.github.com>
2026-03-10 21:40:55 -0400

    Swap out hardcoded id with config option

--- src/modules/logs.js
+++ src/modules/logs.js
@@ -30,8 +30,8 @@ module.exports = ({ bot, knex, config, commands, hooks }) => {
     const channel = await getOrFetchChannel(bot, msg.channel.id);
     let userThreads = await threads.getClosedThreadsByUserId(userId);
 
-    // Command isn't being used in an admin channel or thread
-    if (! [msg.channel.parentID, msg.channel.id].includes('370708369951948802')) {
+    // Command isn't being used in an admin channel or thread '370708369951948802'
+    if (! [msg.channel.parentID, msg.channel.id].includes(config.adminChannelId)) {
       userThreads = userThreads.filter((t) => ! t.isPrivate);
     }
 
