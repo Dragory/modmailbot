d44134acafffd2c83617bc50468959750b6e1e2f
Dragory <2606411+Dragory@users.noreply.github.com>
2026-07-27 20:41:16 +0000

    chore: remove unnecessary mv package

--- src/data/attachments.js
+++ src/data/attachments.js
@@ -1,11 +1,11 @@
 const Eris = require("eris");
 const fs = require("fs");
+const fsp = require("fs/promises");
 const https = require("https");
 const {promisify} = require("util");
 const tmp = require("tmp");
 const config = require("../cfg");
 const utils = require("../utils");
-const mv = promisify(require("mv"));
 const path = require("path");
 
 const getUtils = () => require("../utils");
@@ -95,7 +95,7 @@ saveLocalAttachment = async (attachment) => {
   const downloadResult = await downloadAttachment(attachment);
 
   // Move the temp file to the attachment folder
-  await mv(downloadResult.path, targetPath);
+  await fsp.rename(downloadResult.path, targetPath);
 
   // Resolve the attachment URL
   const url = await getLocalAttachmentUrl(attachment.id, attachment.filename);
